e410cd8ee48179aa44be9d3c68cac0fab0abee79
Ishaan Agarwal <73911978+IshaanA-projects@users.noreply.github.com>
2026-07-15 19:10:41 +0100

    Refactor PongBot episode handling logic
    Refactor episode loop to use a while loop with state input concatenation and handle termination conditions.

--- PongBot/PongBotTesting.py
+++ PongBot/PongBotTesting.py
@@ -7,7 +7,8 @@ import torch.optim as optim
 from torch.distributions import Bernoulli
 import torch.nn.functional as F
 
-episode_length = 1000
+episodes = 5
+
 class PongBot(nn.Module):
     def __init__(self):
         super().__init__()
@@ -39,30 +40,29 @@ model.eval()
 gym.register_envs(ale_py)
 env = gym.make("ALE/Pong-v5", render_mode = "human", obs_type = "ram")
 
-state = prepro(env.reset()[0])
-init_state = prepro(np.zeros(128))
 state_pool = []
 action_pool = []
 reward_pool = []
 
-for j in range(episode_length):
-    if j == 0:
-        prob = model.forward(init_state)
-        m = Bernoulli(prob).sample()
-        action = m.numpy().astype(int).item() + 2
-        state_pool.append(state)
-        step = env.step(action)
-        state = prepro(step[0])
-        reward = step[1]
-        action_pool.append(action)
-        reward_pool.append(reward)
-    else:
-        prob = model.forward(state - state_pool[j - 1])
-        m = Bernoulli(prob).sample()
-        action = m.numpy().astype(int).item() + 2
-        state_pool.append(state)
-        step = env.step(action)
-        state = prepro(step[0])
-        reward = step[1]
-        action_pool.append(action)
-        reward_pool.append(reward)
+state = prepro(env.reset()[0])
+prev_state = torch.zeros_like(state)
+
+done = False
+
+while not done:
+
+    state_input = torch.cat((state, prev_state), dim=1)
+
+    with torch.no_grad():
+        prob = model(state_input)
+        action = Bernoulli(prob).sample()
+
+    action = action.item() + 2
+
+    prev_state = state.clone()
+
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    state = prepro(observation)
+
+    done = terminated or truncated
