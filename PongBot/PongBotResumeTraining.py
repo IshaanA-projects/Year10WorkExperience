f28276c5a7c829a5c93e7f79cf5fd40a9fc964a3
Ishaan Agarwal <73911978+IshaanA-projects@users.noreply.github.com>
2026-07-17 15:55:57 +0100

    Fixing accidental addition of actor-critic model

--- PongBot/PongBotResumeTraining.py
+++ PongBot/PongBotResumeTraining.py
@@ -5,12 +5,14 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.distributions import Bernoulli
+import torch.nn.functional as F
 
 discount_factor = 0.99
 alpha = 1e-4
 episodes = 10000
 batch_size = 10
 raw_reward = []
+
 
 class PongBot(nn.Module):
     def __init__(self):
@@ -19,25 +21,12 @@ class PongBot(nn.Module):
         self.W2 = nn.Linear(200, 100)
         self.W3 = nn.Linear(100, 1)
 
-
     def forward(self, x):
-        x = torch.relu(self.W1(x))
-        x = torch.relu(self.W2(x))
-        x = torch.sigmoid(self.W3(x))
+        x = F.relu(self.W1(x))
+        x = F.relu(self.W2(x))
+        x = F.sigmoid(self.W3(x))
         return x
 
-class ValueNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.W1 = nn.Linear(256, 200)
-        self.W2 = nn.Linear(200, 200)
-        self.W3 = nn.Linear(200, 1)
-
-    def forward(self, x):
-        x = torch.relu(self.W1(x))
-        x = torch.relu(self.W2(x))
-        x = self.W3(x)
-        return x
 
 def prepro(I):
     I = torch.from_numpy(I).float()
@@ -47,21 +36,20 @@ def prepro(I):
 
 
 gym.register_envs(ale_py)
-env = gym.make("ALE/Pong-v5", obs_type = "ram")
+env = gym.make("ALE/Pong-v5", obs_type="ram")
 
-actor = PongBot()
-critic = ValueNet()
-actor_optimizer = optim.RMSprop(actor.parameters(), lr = alpha)
-critic_optimizer = optim.RMSprop(critic.parameters(), lr = alpha)
-mse = nn.MSELoss()
+model = PongBot()
+model = torch.load(r"bot.pt", weights_only=False)
+model.train()
+
+
+optimizer = optim.RMSprop(model.parameters(), lr=alpha)
 
 for i in range(episodes):
     # New episode
     if i % batch_size == 0:
-        actor_optimizer.zero_grad()
-        critic_optimizer.zero_grad()
-        critic_losses = []
-
+        optimizer.zero_grad()
+        losses = 0
     state = prepro(env.reset()[0])
     prev_state = torch.zeros_like(state)
     state_pool = []
@@ -70,13 +58,13 @@ for i in range(episodes):
     done = False
 
     while not done:
-        state_input = torch.cat((state, prev_state), dim = 1)
+        state_input = torch.cat((state, prev_state), dim=1)
         with torch.no_grad():
-            prob = actor(state_input)
+            prob = model.forward(state_input)
             m = Bernoulli(prob).sample()
         action = int(m.item()) + 2
-        state_pool.append(state.clone().detach())
-        prev_state = state.clone().detach()
+        state_pool.append(state.clone())
+        prev_state = state.clone()
         step = env.step(action)
         state = prepro(step[0])
         reward = step[1]
@@ -96,41 +84,42 @@ for i in range(episodes):
         running_reward = running_reward * discount_factor + reward_pool[r]
         reward_pool[r] = running_reward
 
+    # Normalising the reward pool (So that half of actions are punished and half are rewarded)
+    reward_pool = np.array(reward_pool)
+    reward_pool -= np.mean(reward_pool)
+    reward_pool /= np.max([np.std(reward_pool), 1e-2])
+
     # Gradient descent
     for s in range(len(reward_pool)):
         if s == 0:
             prev_state = torch.zeros_like(state_pool[s])
         else:
-            prev_state = state_pool[s-1]
+            prev_state = state_pool[s - 1]
 
-        state_input = torch.cat((state_pool[s], prev_state), dim = 1)
-        prob = actor(state_input)
+        state_input = torch.cat((state_pool[s], prev_state), dim=1)
+        prob = model.forward(state_input)
         m = Bernoulli(prob)
-        action = torch.tensor([[float(action_pool[s] - 2)]])
-        reward = torch.tensor([[float(reward_pool[s])]])
-        baseline = critic(state_input)
-        critic_loss = mse(baseline, reward)
-        advantage = reward - baseline.detach()
-        actor_loss = -1 * m.log_prob(action) * advantage
-        critic_losses.append(critic_loss.item())
-        loss = actor_loss + critic_loss
+        action = torch.tensor(
+            [[float(action_pool[s] - 2)]]
+        )
+        reward = reward_pool[s]
+
+        loss = -1 * m.log_prob(action) * reward
+        losses += loss.item()
         loss.backward()
 
     if (i + 1) % batch_size == 0:
-        print(f"Episode : {i + 1}")
-        actor_optimizer.step()
-        critic_optimizer.step()
-        print(f"Critic loss : {np.mean(critic_losses)}")
+        optimizer.step()
+        print(losses)
 
     if (i + 1) % 100 == 0:
-        print(f"Average Reward : {sum(raw_reward) / len(raw_reward)}")
-        print(f"Highest Reward : {max(raw_reward)}")
-        print(f"Lowest Reward : {min(raw_reward)}")
+        print(f"Episode : {i + 1}")
+        print(f"Average raw reward: {sum(raw_reward) / 100}")
+        print(f"Highest raw reward: {max(raw_reward)}")
+        print(f"Lowest raw reward: {min(raw_reward)}")
         raw_reward = []
-    if (i + 1) % 500 == 0:   # Regular model saving to allow for the program to be run without fear of losing model parameters
-        torch.save(actor, r"actor.pt")
-        torch.save(critic, r"critic.pt")
+    if (i + 1) % 500 == 0:
+        torch.save(model,
+                   r"bot.pt")  # Regular model saving to allow for the program to be run without fear of losing model parameters
 
-
-torch.save(actor, r"actor.pt")
-torch.save(critic, r"critic.pt")
+torch.save(model, r"bot.pt")
